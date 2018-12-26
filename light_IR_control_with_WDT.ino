fb392c34873c7f9eb80f8610e4e283889c704610
kloky777 <34897057+kloky777@users.noreply.github.com>
2018-12-26 15:12:11 +0100

    Update light_IR_control_with_WDT.ino

--- light_IR_control_with_WDT.ino
+++ light_IR_control_with_WDT.ino
@@ -1,9 +1,10 @@
 /*
 This program is created by Andrej Klochan
 The program is controling the process of light bulb switching on and off, moreover, the charging of the battery is controlled by this program
-The last update of this program: 31.3.2018
-The last changes made in this program: Program fixes a hardware failures of the charging relay of the battery - if the relay is stuck, the program manages to reset it
+The last update of this program: 26.12.2018
+The last changes made in this program: recharging battery condition, if the battery is not fully charged after 2.5 hours.
 This changes are made in void charging() function
+Add void checkRelayStuck() function - checks, whether relay is not stuck after charging.
 */
 
 #include <avr/wdt.h>
@@ -27,6 +28,7 @@ volatile boolean f_pcint0=1;
 int counter=111;//this variable has to be set close to 113 in the start of the program, because of the charging function - checking, wether the charging of the battery has to be done
 int bcounter=0;
 int state=0;
+boolean charged = false;    //assumption: battery is not charged above 4.1V at the start
 
 IRrecv irrecv(RECV_PIN);
 decode_results results;
@@ -97,6 +99,7 @@ void loop()
          {
     
             charging();
+            checkRelayStuck();
             counter = 0;
                           }
       else 
@@ -128,32 +131,60 @@ void charging()    //This part of program runs every 15minues
 //------------------------------------------------------
  int pin_baterka = digitalRead(0);
  int battery = analogRead(A2);
- if (battery <=690)  // If the battery has lower output voltage than 3.37V, this condition is fulfilled
+ if (battery <=690)  // If the battery has lower output voltage than 3.37V, starts charging
 {
 digitalWrite(baterka,HIGH);        // So the charging relay is switched on
 bcounter=0;                        // And the bcounter is reseted
+charged = false;        // battery is not charged
+
                           }
-if (battery>=840 && pin_baterka==LOW)    // This condition is fulfilled, if the charging relay is stuck, it means that AC charger charges the battery for a long time
+
+//------------------------------------------------------
+if(bcounter >= 10 && pin_baterka==HIGH)        // 15minutes multiplied by ten is equal to two and a half of an hour
+{
+digitalWrite(baterka,LOW);   // after 2,5hours of charging, the charging relay is switched off
+bcounter=0;
+charged = true;           //this has to be true only if pin_baterka is HIGH
+}
+else if (bcounter >=10 && pin_baterka == LOW)
+{
+charged = false;
+bcounter++;
+}
+else {
+  bcounter++;
+  }
+
+if(charged == true && battery <= 820 && bcounter >=1) {
+  charged = false;
+  digitalWrite(baterka, HIGH);
+  bcounter = 6;
+  }
+//------------------------------------------------------
+cbi(ADCSRA,ADEN); // Switch Analog to Digital converter OFF
+//------------------------------------------------------
+}
+
+void checkRelayStuck() {
+  //------------------------------------------------------
+ sbi(ADCSRA,ADEN);  // Switch Analog to Digital converter ON
+//------------------------------------------------------
+  
+  int pin_baterka = digitalRead(0);
+  int battery = analogRead(A2);
+  if (battery>=840 && pin_baterka==LOW)    // This condition is fulfilled, if the charging relay is stuck, it means that AC charger charges the battery for a long time
 {
 digitalWrite(baterka,HIGH);                          // Hence, the charging relay is going to be switched on anyway
 delay(2000);
 digitalWrite(baterka,LOW);                            // and after two seconds off, in order to reset the relay
                               }
 //------------------------------------------------------
-if(bcounter>=10)        // 15minutes multiplied by ten is equal to two and a half of an hour
-{
-digitalWrite(baterka,LOW);   // after 2,5hours of charging, the charging relay is switched off
-bcounter=0;
-}
-else
-{
-bcounter++;
-}
-
-//------------------------------------------------------
 cbi(ADCSRA,ADEN); // Switch Analog to Digital converter OFF
 //------------------------------------------------------
-}
+  
+  }
+
+
 
 //****************************************************************
 // 0=16ms, 1=32ms,2=64ms,3=128ms,4=250ms,5=500ms
